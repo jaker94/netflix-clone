4b5144724a8597bb8ddb5b43a6dd82df4ddd0371
Jacob <jakeregister94@gmail.com>
2023-03-13 16:47:38 -0600

    header/nav completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,29 @@
-import { FaSearch } from 'react-icons/fa'
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { FaBell, FaSearch } from "react-icons/fa";
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      }
+      else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
   return (
-    <header>
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://rb.gy/ulxxee"
@@ -19,8 +40,17 @@ function Header() {
         </ul>
       </div>
 
-      <div>
-     <FaSearch />
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <FaSearch className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <FaBell className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
       </div>
     </header>
   );
