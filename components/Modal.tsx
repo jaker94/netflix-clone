25cbc662ed6bb39418342aed052f735ccb57ef59
Jacob <jakeregister94@gmail.com>
2023-04-28 12:04:01 -0600

    truncate

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -189,8 +189,8 @@ function Modal() {
               </div>
             </div>
           </div>
-          <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row sm:h-[calc(50%)]">
-            <p className="w-5/6 sm:truncate">{movie?.overview}</p>
+          <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row sm:h-40vh">
+            <p className="w-5/6 truncate ...">{movie?.overview}</p>
             <div>
               <div className="flex flex-col space-y-3 text-sm">
                 <span className="text-[gray]">Genres:</span>
